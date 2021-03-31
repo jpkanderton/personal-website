3b7e587c179d8ac742feef5273b2fa457aed1a01
John Anderton <janderton24@gmail.com>
2021-03-31 10:44:22 -0600

    adds a description detailing what the about button does to the body's overflow when clicked

--- client/dist/jqueryScripts.js
+++ client/dist/jqueryScripts.js
@@ -12,7 +12,7 @@ $( "#main-icon-container" ).click(function() {
   }
 });
 
-// DESCRIPTION on click of the main icon, the overflow is hidden / visible
+// DESCRIPTION on click of the "about" div element (denoted by the grid-col-start class), the body element's overflow is hidden / visible
 $( ".grid-col-start-6" ).click(function() {
   if( $("body").css('overflow') == 'visible') {
     $("body").css("overflow", "hidden");
