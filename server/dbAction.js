0cce9fb9bee779dc003e804e65e5207b03212217
John Anderton <janderton24@gmail.com>
2021-04-27 09:24:12 -0600

    adds mysql2 databes with table contact_messages

---
+++ server/dbAction.js
@@ -0,0 +1,3 @@
+var connection = './dbConfig.js';
+
+connection.
---
+++ server/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS portfolio_website;
+
+USE portfolio_website;
+
+CREATE TABLE IF NOT EXISTS contact_messages (
+  message_id INT AUTO_INCREMENT,
+  date_time NOW(),
+  name VARCHAR(250),
+  email VARCHAR(250),
+  message_content VARCHAR(2000)
+)
