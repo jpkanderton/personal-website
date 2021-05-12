f0407854463efc28f345d65fe63171173564611b
John Anderton <janderton24@gmail.com>
2021-05-12 13:36:12 -0600

    removes the unecessary jquery scripts

--- client/dist/jqueryScripts.js
+++ client/dist/jqueryScripts.js
@@ -8,37 +8,7 @@ $( "#main-icon-container" ).click(function() {
   $("body").css("overflow", "visible")
 });
 
-// DESCRIPTION on click of the "about" div element (denoted by the grid-col-start class), the body element's overflow is hidden / visible
-// $( ".grid-col-start-6" ).click(function() {
-//   if( $("body").css('overflow') == 'visible') {
-//     $("body").css("overflow", "hidden");
-//   } else {
-//     $("body").css("overflow", "visible");
-//   }
-// });
-
-
-
+// DESCRIPTION on click of .grid-col-start-4 (the Home button) the overflow is hidden
 $( ".grid-col-start-4" ).click(function() {
   $("body").css("overflow", "hidden");
-});
-
-// onclick of email, copies to key board
-
-// const email = document.getElementById("contact-page-email");
-
-// $( "#contact-page-email" ).click(function() {
-//   console.log('hihihi')
-//   email.text();
-//   document.execCommand("copy");
-//   alert("Copied the text: " + copyText.value);
-//   console.log('copied text')
-// });
-
-// $( "#contact-additional-email-container" ).click(function() {
-//   $("#contact-additional-email-container").css("background-color", "red");
-// });
-$( "#contact-main-container" ).click(function() {
-  console.log('hi i am here')
-  $("#landing-container").css("background-color", "red");
 });
