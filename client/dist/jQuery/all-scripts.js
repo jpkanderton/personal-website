35fd25c9786de187692a303c57da42edfe94cd9e
John Anderton <janderton24@gmail.com>
2021-05-25 10:34:03 -0600

    removes unecessary img files and adds a new jquery folder

---
+++ client/dist/jQuery/all-scripts.js
@@ -0,0 +1,14 @@
+// DESCRIPTION on click of landing page's inner square, the overflow is hidden
+$( "#landing-inner" ).click(function() {
+  $("body").css("overflow", "visible");
+});
+
+// DESCRIPTION on click of the main icon, the overflow is hidden / visible
+$( "#main-icon-container" ).click(function() {
+  $("body").css("overflow", "visible")
+});
+
+// DESCRIPTION on click of .grid-col-start-4 (the Home button) the overflow is hidden
+$( ".grid-col-start-4" ).click(function() {
+  $("body").css("overflow", "hidden");
+});
